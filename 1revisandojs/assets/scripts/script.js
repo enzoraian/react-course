d8899451c179b110721d776fe7dc4a3f52b4642a
enzoraian <enzo.candido@ccc.ufcg.edu.br>
2025-10-30 06:23:44 -0300

    primeiro commit

---
+++ 1revisandojs/assets/scripts/script.js
@@ -0,0 +1,11 @@
+// 1) Import e Export
+// import - pega elementos externos para usar dentro desse elemento
+
+import { soma } from "./util.js";
+import { apiKey } from "./util.js";
+
+console.log(soma(5, 6));
+console.log(apiKey);
+
+// 2) Variáveis e Tipos de Dados
+let str = "Olá Mundo";
---
+++ 1revisandojs/assets/scripts/util.js
@@ -0,0 +1,7 @@
+export function soma(a, b) {
+  return a + b;
+}
+
+export let apiKey = "12781278asnaejhkqwejekqeqkl";
+
+// export - deixa elemento disponível para ser importado em outros elementos
